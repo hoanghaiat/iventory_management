1a2c1aa4b9391726538b5adb5ea0302ea4ce1624
Tran Dat Huy <huyuy1682@gmail.com>
2025-08-12 00:40:59 +0700

    update

---
+++ KienTruc_University/locator.py
@@ -0,0 +1,11 @@
+#login
+USERNAME_INPUT="//input[@id='id_username']"
+PASSWORD_INPUT="//input[@id='id_password']"
+LOGIN_BUTTON="//button[text()='Đăng nhập']"
+#thaotacnhanh
+BTN_coursemanagement="//a[contains(@class,'btn-success') and @href='/en/academics/courses/']"
+Menu_HP="//a[@href='/en/academics/courses/']"
+BTN_TEST="//a[contains(@class,'btn-warning') and @href='/en/assessment/tests/']"
+Menu_BKT="//a[@href='/en/assessment/tests/' and contains(@class,'menu-item')]"
+BTN_HOME="//nav[@aria-label='breadcrumb']//a[@href='/en/' and normalize-space()='Trang chủ']"
+Menu_HOME="//a[@href='/en/' and contains(@class,'menu-item')]"
--- KienTruc_University/test.robot
+++ KienTruc_University/test.robot
@@ -6,4 +6,11 @@ Resource    resource.resource
 
 *** Test Cases ***
 Login
-    Login To System    admin    demoadmin
+    Login To System    lecturer1    demolecturer
+TC_003_ThaoTacNhanh
+    [Documentation]    Đăng nhập và truy cập trang học phần từ Thao tác nhanh
+    Go To Course Management
+    Go To Home Page
+    Go To Course Test
+    Sleep    5s
+    [Teardown]    Close Browser
